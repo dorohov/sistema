265d5413917352c339c8b368a533974092a4556c
Pavel Proyavin <pavel@dorohovdesign.com>
2019-08-01 17:50:19 +0300

    changelog from 010819

--- src/js/directions.js
+++ src/js/directions.js
@@ -2,19 +2,49 @@
     "use strict"
     $(function() {
 
-        var swiper = new Swiper('.directions__center__inner', {
-            spaceBetween: 30,
-            slidesPerView:  2,
-            effect: 'fade',
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true,
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          });
+      function setSlideInfo(slideId) {
+
+        var currentSlide = $('.directions__center__carousel__item[data-id="' + slideId + '"]')[0],
+        currentInfo = {
+          id: slideId,
+          title: $(currentSlide).data('title'),
+          text: $(currentSlide).data('text'),
+          link: $(currentSlide).data('link')
+        }
+
+        $('.directions__right__heading').html(currentInfo.title)
+        $('.directions__right__text').html(currentInfo.text)
+        $('.directions__right__btn a').attr('href', currentInfo.link)
+
+        $('.directions__menu ul li').removeClass('is--active')
+        $('.directions__menu ul li[data-id="' + currentInfo.id + '"]').addClass('is--active')
+
+      }
+
+      function setSlide(slideId) {
+        swiper.slideTo(slideId + 1)
+      }
+
+      var swiper = new Swiper('.directions__center__inner', {
+        spaceBetween: 30,
+        slidesPerView:  2,
+        effect: 'fade',
+        loop: true,
+        touchRatio: 0,
+        on: {
+          init: function() {
+            setSlideInfo(0)
+          },
+          slideChange: function() {
+            setSlideInfo(this.realIndex)
+          }
+        }
+      });
+
+      $('.directions__menu ul li').on('click', function() {
+        var thisId = $(this).data('id')
+        setSlide(thisId)
+      })
 
     })
 })(jQuery);
