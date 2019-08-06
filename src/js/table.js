455cf54abcfd6c186f0498fbd580159dfe543865
Pavel Proyavin <pavel@dorohovdesign.com>
2019-08-06 15:17:28 +0300

    changelog from 060819

--- src/js/table.js
+++ src/js/table.js
@@ -4,8 +4,46 @@
 
         $('.table__list__items').slick({
             slidesToShow: 7,
-            prevArrow: '',
-            nextArrow: ''
+            prevArrow: '.table__controls button.is--prev',
+            nextArrow: '.table__controls button.is--next',
+            slidesToScroll: 7,
+            responsive: [
+                {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 6
+                    }
+                },
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 440,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         })
 
     })
