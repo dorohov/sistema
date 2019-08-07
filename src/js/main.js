8d8a9d60de9ef2aa6f77ed2435781be40409e583
Pavel Proyavin <pavel@dorohovdesign.com>
2019-08-07 17:41:59 +0300

    changelog from 070819

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,21 @@
 
             var classes = {
                 paddingLeft: '.is--c-pl',
-                paddingRight: '.is--c-pr'
+                paddingRight: '.is--c-pr',
+                height100Per: '.is--h100'
             }
 
             var padding = document.getElementsByClassName('navbar__inner')[0].getBoundingClientRect()
+            var height = $('.navbar').innerHeight() + $('.footer').innerHeight()
 
             $(classes.paddingLeft).css({
                 paddingLeft: padding.left + 30
             })
             $(classes.paddingRight).css({
                 paddingRight: padding.left + 30
+            })
+            $(classes.height100Per).css({
+                minHeight: 'calc(100vh - ' + height + 'px)'
             })
 
         }
---
+++ src/js/map.js
@@ -0,0 +1,18 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [52.971233, 36.054100],
+            zoom: 17,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        })
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([52.971233, 36.054100], {
+            balloonContent: 'Фитнес Клуб <strong>Система Комплекс</strong>'
+        }, {
+            preset: 'islands#redSportIcon'
+        }));
+}
